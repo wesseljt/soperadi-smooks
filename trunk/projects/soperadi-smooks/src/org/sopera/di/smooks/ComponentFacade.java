814806e1f22a32d83e69bb08a572274befa66997
renat.zubairov <renat.zubairov@1032afae-8658-11de-9a40-4546843651d6>
2009-09-08 09:09:08 +0000

    Initial project structure with ComponentFacade and it's implementation. Added google-guice for handling dependency injection and increase testability.

---
+++ trunk/projects/soperadi-smooks/src/org/sopera/di/smooks/ComponentFacade.java
@@ -0,0 +1,29 @@
+/*******************************************************************************
+ * Copyright (c) 2009 SOPERA GmbH
+ * All rights reserved. 
+ * This program and the accompanying materials are made available
+ * under the terms of the GNU Lesser General Public License v 3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-3.0.txt
+ ******************************************************************************/
+package org.sopera.di.smooks;
+
+import org.sopera.di.smooks.impl.ComponentFacadeImpl;
+
+/**
+ * Component facade for interaction with Talend generated job.
+ * 
+ * This interface will be a primary facade that will be used
+ * by Talend-Smooks jobs at runtime.
+ * 
+ * @author zubairov
+ *
+ */
+public interface ComponentFacade {
+
+	/**
+	 * Singleton instance
+	 */
+	public static final ComponentFacade INSTANCE = new ComponentFacadeImpl();
+	
+}
