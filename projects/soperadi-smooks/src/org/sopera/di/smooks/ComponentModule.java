a709829d72e5f2a1fc4eb212b08498f6639637e7
renat.zubairov <renat.zubairov@1032afae-8658-11de-9a40-4546843651d6>
2009-09-08 09:09:08 +0000

    Initial project structure with ComponentFacade and it's implementation. Added google-guice for handling dependency injection and increase testability.

---
+++ projects/soperadi-smooks/src/org/sopera/di/smooks/ComponentModule.java
@@ -0,0 +1,29 @@
+/*******************************************************************************
+ * Copyright (c) 2009 SOPERA GmbH
+ * All rights reserved. 
+ * This program and the accompanying materials are made available
+ * under the terms of the GNU Lesser General Public License v 3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-3.0.txt
+ ******************************************************************************/
+package org.sopera.di.smooks;
+
+import com.google.inject.AbstractModule;
+
+/**
+ * Module that will configure injection rules
+ * 
+ * @author zubairov
+ * @see AbstractModule
+ */
+public class ComponentModule extends AbstractModule {
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected void configure() {
+		// TODO Auto-generated method stub
+	}
+
+}
---
+++ projects/soperadi-smooks/src/org/sopera/di/smooks/impl/ComponentFacadeImpl.java
@@ -0,0 +1,21 @@
+/*******************************************************************************
+ * Copyright (c) 2009 SOPERA GmbH
+ * All rights reserved. 
+ * This program and the accompanying materials are made available
+ * under the terms of the GNU Lesser General Public License v 3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-3.0.txt
+ ******************************************************************************/
+
+package org.sopera.di.smooks.impl;
+
+import org.sopera.di.smooks.ComponentFacade;
+
+/**
+ * Default implementation of the {@link ComponentFacade}
+ * 
+ * @author zubairov
+ */
+public class ComponentFacadeImpl implements ComponentFacade {
+
+}
