47413db94d67f4a8d61a586ff7e7382a6f3fa6ad
renat.zubairov <renat.zubairov@1032afae-8658-11de-9a40-4546843651d6>
2009-09-14 08:00:25 +0000

    intermediate commit to synchronize state

--- trunk/projects/soperadi-smooks/src/org/sopera/di/smooks/xpath/impl/SAXPathExpressionBuilderImpl.java
+++ trunk/projects/soperadi-smooks/src/org/sopera/di/smooks/xpath/impl/SAXPathExpressionBuilderImpl.java
@@ -17,6 +17,19 @@
 
 package org.sopera.di.smooks.xpath.impl;
 
+import java.util.List;
+import java.util.Map;
+
+import org.jaxen.JaxenHandler;
+import org.jaxen.expr.Expr;
+import org.jaxen.expr.LocationPath;
+import org.jaxen.expr.NameStep;
+import org.jaxen.expr.Step;
+import org.jaxen.expr.UnionExpr;
+import org.jaxen.expr.XPathExpr;
+import org.jaxen.saxpath.Axis;
+import org.jaxen.saxpath.SAXPathException;
+import org.jaxen.saxpath.XPathReader;
 import org.sopera.di.smooks.xpath.SAXPathExpressionBuilder;
 import org.sopera.di.smooks.xpath.SXPathExpr;
 
@@ -27,12 +40,66 @@ import org.sopera.di.smooks.xpath.SXPathExpr;
  */
 public class SAXPathExpressionBuilderImpl implements SAXPathExpressionBuilder {
 
+	private final XPathReader reader = new org.jaxen.saxpath.base.XPathReader();
+
+	public Map<String, String> namespaces;
+	
 	/**
-	 * @{inheritDoc}
+	 * {@inheritDoc}
 	 */
-	public SXPathExpr buildExpression(String xPath) {
-		// TODO Auto-generated method stub
+	public SXPathExpr buildExpression(String xPath) throws SAXPathException {
+		JaxenHandler handler = new JaxenHandler();
+		reader.setXPathHandler(handler);
+		reader.parse(xPath);
+		XPathExpr xpath = handler.getXPathExpr();
+		return visitExpr(xpath.getRootExpr());
+	}
+
+	/**
+	 * Start visiting
+	 * 
+	 * @param expr
+	 */
+	public SXPathExpr visitExpr(Expr expr) {
+		if (expr instanceof UnionExpr) {
+			UnionExpr unionExpr = (UnionExpr) expr;
+			SXPathExpr left = visitExpr(unionExpr.getLHS());
+			SXPathExpr right = visitExpr(unionExpr.getRHS());
+			return new org.sopera.di.smooks.xpath.expr.UnionExpr(left, right);
+		}
+		if (expr instanceof LocationPath) {
+			LocationPath path = (LocationPath) expr;
+			return visitPath(path);
+		}
+		throw new RuntimeException("Unuspported type of expression " + expr.getClass() + ":" + expr);
+	}
+
+	/**
+	 * @param path
+	 */
+	@SuppressWarnings("unchecked")
+	private SXPathExpr visitPath(LocationPath path) {
+		List<Step> steps = path.getSteps();
+		System.err.println("Absolute: " + path.isAbsolute());
+		for (Step step : steps) {
+			if (step instanceof NameStep) {
+				NameStep nameStep = (NameStep) step;
+				System.err.println("Selecting "
+						+ Axis.lookup(step.getAxis()) + " with "
+						+ nameStep.getPrefix() + ":"
+						+ nameStep.getLocalName());
+			} else {
+				System.err.println("UnknownType: " + step.getClass() + " - " + step);
+			}
+		}
 		return null;
 	}
 
+	/**
+	 * @param namespaces the namespaces to set
+	 */
+	public void setNamespacesMap(Map<String, String> namespaces) {
+		this.namespaces = namespaces;
+	}
+	
 }
