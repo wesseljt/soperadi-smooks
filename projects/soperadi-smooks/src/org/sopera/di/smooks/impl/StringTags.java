0f08273cd2f1b378a9836561a5096e990e8c3489
Alexander.G.Osadchiy <Alexander.G.Osadchiy@1032afae-8658-11de-9a40-4546843651d6>
2009-09-18 15:01:28 +0000

    This is a resource synchronization class. It will be used for read and write threads synchronization.

---
+++ projects/soperadi-smooks/src/org/sopera/di/smooks/impl/StringTags.java
@@ -0,0 +1,52 @@
+package org.sopera.di.smooks.impl;
+
+import java.util.HashMap;
+
+public class StringTags {
+	
+	HashMap<String, String> tags = new HashMap<String, String>();;
+	boolean state = false;
+
+	String findTagValue(String tagName) {
+		// System.out.println("read: " + tags.get(tagName));
+		return tags.get(tagName);
+	}
+
+	void write(String key, String res) {
+		this.tags.put(key, res);
+		// System.out.println("write: " + key + "    " + res);
+	}
+
+	synchronized void startWrite() {
+		if (state) {
+			try {
+				wait();
+			} catch (InterruptedException e) {
+			}
+		}
+	}
+
+	synchronized void startRead() {
+		if (!state) {
+			try {
+				wait();
+			} catch (InterruptedException e) {
+			}
+		}
+	}
+
+	synchronized void endRead() {
+		if (state) {
+			state = false;
+			notify();
+		}
+	}
+
+	synchronized void endWrite() {
+		if (!state) {
+			state = true;
+			notify();
+		}
+	}
+}
+
