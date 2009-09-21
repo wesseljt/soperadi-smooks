c35d5243255746d5d319fd07930d9278c10a4dc6
rnminko <rnminko@1032afae-8658-11de-9a40-4546843651d6>
2009-09-21 07:46:45 +0000

    Test classes for first component

--- trunk/projects/soperadi-smooks/src/org/sopera/di/smooks/impl/StringTags.java
+++ trunk/projects/soperadi-smooks/src/org/sopera/di/smooks/impl/StringTags.java
@@ -2,51 +2,59 @@ package org.sopera.di.smooks.impl;
 
 import java.util.HashMap;
 
-public class StringTags {
-	
-	HashMap<String, String> tags = new HashMap<String, String>();;
+public class StringTags 
+{
+	HashMap<String,String> tags = new HashMap<String,String>();;
 	boolean state = false;
-
-	String findTagValue(String tagName) {
-		// System.out.println("read: " + tags.get(tagName));
+	
+	String findTagValue(String tagName)
+	{
+		System.out.println("read: "+tags.get(tagName));
 		return tags.get(tagName);
 	}
-
-	void write(String key, String res) {
+	
+	void write(String key, String res)
+	{
 		this.tags.put(key, res);
-		// System.out.println("write: " + key + "    " + res);
+		System.out.println("write: "+key+"    "+res);
 	}
-
-	synchronized void startWrite() {
-		if (state) {
-			try {
+	
+	synchronized void startWrite()
+	{
+		if(state)
+		{
+			try
+			{
 				wait();
-			} catch (InterruptedException e) {
 			}
+			catch(InterruptedException e){}
 		}
 	}
-
-	synchronized void startRead() {
-		if (!state) {
-			try {
+	
+	synchronized void startRead()
+	{
+		if(!state)
+		{
+			try
+			{
 				wait();
-			} catch (InterruptedException e) {
 			}
+			catch(InterruptedException e){}
 		}
 	}
-
-	synchronized void endRead() {
-		if (state) {
+	synchronized void endRead()
+	{
+		if(state) {
 			state = false;
 			notify();
 		}
 	}
-
-	synchronized void endWrite() {
-		if (!state) {
+	
+	synchronized void endWrite()
+	{
+		if(!state) {
 			state = true;
 			notify();
 		}
 	}
 }
-
