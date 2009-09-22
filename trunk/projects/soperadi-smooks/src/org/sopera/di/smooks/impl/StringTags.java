eb1abc5865c0d622e9a0524f2b5ff9c5c2949061
Alexander.G.Osadchiy <Alexander.G.Osadchiy@1032afae-8658-11de-9a40-4546843651d6>
2009-09-22 14:48:33 +0000

    javadocs

--- trunk/projects/soperadi-smooks/src/org/sopera/di/smooks/impl/StringTags.java
+++ trunk/projects/soperadi-smooks/src/org/sopera/di/smooks/impl/StringTags.java
@@ -2,66 +2,109 @@ package org.sopera.di.smooks.impl;
 
 import java.util.HashMap;
 
-public class StringTags 
-{
-	HashMap<String,String> tags = new HashMap<String,String>();;
-	boolean state = false;
-	boolean end = false;
-	
+/**
+ * This class contains a data structure and methods to work with this structure.
+ * The data structure consists of names of tags and corresponding values of these tags.
+ * 
+ * @author soperadi-smooks
+ * 
+ */
+public class StringTags {
+	private HashMap<String, String> tags = new HashMap<String, String>();
+	private boolean state = false;
+	private boolean end = false;
+
+	/**
+	 * Shows if the end of data structure is reached.
+	 * @return boolean value, which shows if we have an end of data flow.
+	 */
 	public boolean isEnd() {
 		return end;
 	}
 
+	/**
+	 *  
+	 * @param end sign of the end of flow.
+	 */
 	public void setEnd(boolean end) {
 		this.end = end;
 	}
 
-	String findTagValue(String tagName)
-	{
-		System.out.println("read: "+tags.get(tagName));
+	/**
+	 * This method finds the value of corresponding tag.
+	 * 
+	 * @param tagName name of tag, which value is needed.
+	 * @return value of corresponding tag.
+	 */
+	public String findTagValue(String tagName) {
+		System.out.println("read: " + tags.get(tagName));
 		return tags.get(tagName);
 	}
-	
-	void write(String key, String res)
-	{
+
+	/**
+	 * Writing of data to the structure.
+	 * 
+	 * Associates the specified tag value with the specified name of tag in data structure.
+	 * If the data structure previously contained a mapping for the tag name, the old value is replaced.
+	 * @param key tag name with which the specified value is to be associated.
+	 * @param res value to be associated with the specified tag name.
+	 */
+	public void write(String key, String res) {
 		this.tags.put(key, res);
-		System.out.println("write: "+key+"    "+res);
+		System.out.println("write: " + key + "    " + res);
 	}
-	
-	synchronized void startWrite()
-	{
-		if(state)
-		{
-			try
-			{
+
+	/**
+	 * Start of writing session.
+	 * 
+	 * If reading session is active, then we wait for the end of it.
+	 * After this we begin writing session.
+	 */
+	public synchronized void startWrite() {
+		if (state) {
+			try {
 				wait();
+			} catch (InterruptedException e) {
 			}
-			catch(InterruptedException e){}
 		}
 	}
-	
-	synchronized void startRead()
-	{
-		if(!state)
-		{
-			try
-			{
+
+	/**
+	 * Start of reading session.
+	 * 
+	 * If writing session is active, then we wait for the end of it.
+	 * After this we begin reading session.
+	 */
+	public synchronized void startRead() {
+		if (!state) {
+			try {
 				wait();
+			} catch (InterruptedException e) {
 			}
-			catch(InterruptedException e){}
 		}
 	}
-	synchronized void endRead()
-	{
-		if(state) {
+
+	/**
+	 * End of reading of data structure.
+	 * 
+	 * This method ends reading session, changes boolean variable, which
+	 * is a state of reading/writing process and notify the writing session.
+	 */
+	public synchronized void endRead() {
+		if (state) {
 			state = false;
 			notify();
 		}
 	}
-	
-	synchronized void endWrite()
-	{
-		if(!state) {
+
+	/**
+	 * End of writing of data structure.
+	 * 
+	 * This method ends writing session, changes boolean variable, which
+	 * is a state of reading/writing process and notify the writing session.
+	 */
+	public synchronized void endWrite() {
+		if (!state) {
 			state = true;
 			notify();
 		}
