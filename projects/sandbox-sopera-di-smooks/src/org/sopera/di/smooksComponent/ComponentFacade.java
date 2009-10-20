b8fdfa01c3f1da7e5a30affd9efcd02b4a2976de
Alexander.G.Osadchiy@gmail.com <Alexander.G.Osadchiy@gmail.com@1032afae-8658-11de-9a40-4546843651d6>
2009-10-20 14:28:47 +0000

    Adding of ComponentFacade classes.

---
+++ projects/sandbox-sopera-di-smooks/src/org/sopera/di/smooksComponent/ComponentFacade.java
@@ -0,0 +1,29 @@
+/*******************************************************************************
+ * Copyright (c) 2009 SOPERA GmbH
+ * All rights reserved. 
+ * This program and the accompanying materials are made available
+ * under the terms of the GNU Lesser General Public License v 3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-3.0.txt
+ ******************************************************************************/
+package org.sopera.di.smooksComponent;
+
+import com.google.inject.Guice;
+
+/**
+ * Component facade for interaction with Talend generated job.
+ * 
+ * This interface will be a primary facade that will be used by Talend-Smooks
+ * jobs at runtime.
+ * 
+ * @author Alexander
+ * 
+ */
+public interface ComponentFacade {
+	
+	/**
+	 * Singleton instance
+	 */
+	public static final ComponentFacade INSTANCE = Guice.createInjector(
+			new ComponentModule()).getInstance(ComponentFacade.class);
+}
