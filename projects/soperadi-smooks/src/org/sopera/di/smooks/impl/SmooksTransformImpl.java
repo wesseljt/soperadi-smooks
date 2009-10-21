1100b29ebbe6d7c30c5ed066d98b9c4d4efa2b99
Alexander.G.Osadchiy@gmail.com <Alexander.G.Osadchiy@gmail.com@1032afae-8658-11de-9a40-4546843651d6>
2009-10-21 13:38:48 +0000

    Changes of SmooksTransformImpl class.

--- projects/soperadi-smooks/src/org/sopera/di/smooks/impl/SmooksTransformImpl.java
+++ projects/soperadi-smooks/src/org/sopera/di/smooks/impl/SmooksTransformImpl.java
@@ -1,5 +1,14 @@
 package org.sopera.di.smooks.impl;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.CharBuffer;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Set;
+
 import org.sopera.di.smooks.SmooksTransform;
 
 public class SmooksTransformImpl implements SmooksTransform{
@@ -7,6 +16,7 @@ public class SmooksTransformImpl implements SmooksTransform{
 	private String inputFileName;
 	private String outputFileName;
 	private String mappingFileName;
+	private HashMap<String, String> mappingResource = new HashMap<String, String>();
 
 	public String getMappingFileName() {
 		return mappingFileName;
@@ -35,7 +45,9 @@ public class SmooksTransformImpl implements SmooksTransform{
 	public boolean runSmooksTransform() throws java.io.IOException,
 			org.xml.sax.SAXException, org.milyn.SmooksException {
 
+		//setMappingResource(targetID, sourseName);
 		// Instantiate Smooks with the config...
+		fillMappingFile();
 		org.milyn.Smooks smooks = new org.milyn.Smooks(getMappingFileName());
 
 		try {
@@ -65,7 +77,30 @@ public class SmooksTransformImpl implements SmooksTransform{
 	}
 
 	public void setMappingResource(String targetID, String sourseName) {
-		// TODO Auto-generated method stub
-		
+		mappingResource.put(targetID, sourseName);
+	}
+	
+	protected void fillMappingFile() {
+		FileReader mappingReader;
+		try {
+			mappingReader = new FileReader(getMappingFileName());
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			throw new RuntimeException("Can't find mapping file");
+		}
+		String file;
+		file = mappingReader.toString();
+		for (Iterator<String> iterator = (mappingResource.keySet()).iterator(); iterator.hasNext();) {
+			String type = (String) iterator.next();
+			file.replaceAll(type, mappingResource.get(type));
+		}
+		FileWriter mappingWriter;
+		try {
+			mappingWriter = new FileWriter(getMappingFileName());
+			mappingWriter.write(file);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
