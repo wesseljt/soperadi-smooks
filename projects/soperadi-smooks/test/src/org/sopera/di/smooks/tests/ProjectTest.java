f282bf19278ec92430d70bc5e5674999c512942e
Alexander.G.Osadchiy <Alexander.G.Osadchiy@1032afae-8658-11de-9a40-4546843651d6>
2009-10-27 11:47:33 +0000

    Added head section of files.

--- projects/soperadi-smooks/test/src/org/sopera/di/smooks/tests/ProjectTest.java
+++ projects/soperadi-smooks/test/src/org/sopera/di/smooks/tests/ProjectTest.java
@@ -1,3 +1,11 @@
+/*******************************************************************************
+ * Copyright (c) 2009 SOPERA GmbH
+ * All rights reserved. 
+ * This program and the accompanying materials are made available
+ * under the terms of the GNU Lesser General Public License v 3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-3.0.txt
+ ******************************************************************************/
 package org.sopera.di.smooks.tests;
 
 import junit.framework.TestCase;
